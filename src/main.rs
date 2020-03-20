237580ec8004517af29e3c75232d68cf17daf5f0
Joel Edwards <joeledwards@gmail.com>
2020-03-20 09:32:25 -0600

    Setup project basics

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    println!("TODO:");
+    println!("- select argument parser (sub-command support)");
+    println!("- select HTTP client carte");
+    println!("- select JSON parser crate");
+    println!("- select CLI color crate");
+    println!("- handle partial URLs (url crate?)");
+    println!("- implement `aqui get`");
+}
